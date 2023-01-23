d0af1d30af99bacfd70c2f11aa03b7139f72c853
lmusil2 <lynette_musil@optum.com>
2023-01-23 17:20:45 +0000

    go variables

---
+++ exported-names.go
@@ -0,0 +1,13 @@
+/* Alta3 Research | RZFeeser
+   Exported names   */
+   
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    fmt.Println(math.pi)   // notice that "Pi" is capitalized
+}
